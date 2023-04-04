503a498fbaab044fb6cab37c67939007d8c48441
James Searle <jamessearle41@hotmail.co.uk>
2023-04-04 11:14:00 +0100

    q4

--- js/app.js
+++ js/app.js
@@ -94,11 +94,21 @@ let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
   //eslint-disable-line
+  const theSum = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
+  const theSumString =
+    sumArr[0] +
+    "," +
+    sumArr[1] +
+    "," +
+    sumArr[2] +
+    " was passed in as an array of numbers, and " +
+    theSum +
+    " is their sum.";
+  return [theSum, theSumString];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
-
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
